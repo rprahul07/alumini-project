e3b8bbcbfb48f8ab318f4dfc33126ade92e85823
Adith Dileep <adithdileepadaditha@gmail.com>
2025-08-23 14:01:19 +0530

    minor fix

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -33,7 +33,7 @@ export default defineConfig(({ command, mode }) => {
       },
       proxy: {
         '/api': {
-          target: process.env.VITE_API_URL || 'http://localhost:5001',
+          target: process.env.VITE_API_BASE_URL || 'http://localhost:5001',
           changeOrigin: true,
           secure: false,
         }
