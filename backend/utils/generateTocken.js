0bd682300b4efeaf23d58375a623f38f7fb236bb
noel-invsto <noel@invsto.com>
2026-01-20 22:37:12 +0530

    same site: none

--- backend/utils/generateTocken.js
+++ backend/utils/generateTocken.js
@@ -12,7 +12,7 @@ const generateTokenAndSetCookie = (userId, role, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
