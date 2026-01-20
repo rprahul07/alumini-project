a4dcd823dc0242ee0b6b8e8341653f224537ca4e
noel-invsto <noelskocheekkaran@gmail.com>
2026-01-20 23:03:44 +0530

    removed the part where console.log is convertingg to 'undefined'

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -214,14 +214,6 @@ export default defineConfig(({ command, mode }) => {
         minifyWhitespace: true
       }
     },
-    define: {
-      // Remove console logs in production
-      ...(isProd ? {
-        'console.log': 'undefined',
-        'console.warn': 'undefined',
-        'console.error': 'undefined'
-      } : {})
-    }
   };
 });
 
